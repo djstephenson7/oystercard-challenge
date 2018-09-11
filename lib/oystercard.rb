f82faf0ac7818aa776aa6965460e184a588eb666
Daniel Stephenson <dan_0502@hotmail.co.uk>
2018-09-11 14:32:18 +0100

    Added top up function

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,12 @@
 
-class OysterCard
+class Oystercard
   attr_reader :balance
+
   def initialize
     @balance = 0
+  end
+
+  def top_up(money)
+    @balance = @balance + money
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,15 @@
 require 'oystercard'
 
-describe OysterCard do
+describe Oystercard do
   describe 'balance' do
     it "checks the balance" do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  describe 'top_up' do
+    it 'adds money' do
+      expect(subject).to respond_to(:top_up).with(1).argument
     end
   end
 end
