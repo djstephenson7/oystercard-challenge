18958a7eb35c8266554ecc4df3184a3e02734dfc
Andres Faraone <andresfaraone@outlook.com>
2018-09-15 11:50:14 +0100

    Tidied up RSpec file

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 group :development, :test do
   gem "rspec"
+  gem "pry"
 end
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,53 +1,46 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station) { double :station }
+  let(:exit_station) { double :station }
+  let(:entry_station) { double :station }
 
-   describe '#initialize' do
-     it 'contains an empty array of journeys' do
-       expect(subject.journey_history).to eq []
-     end
-   end
-  context 'balance on oystercard'
+  describe '#initialize' do
+    it 'contains an empty array of journeys' do
+      expect(subject.journey_history).to eq []
+    end
+  end
+
   describe '#balance' do
     it 'checks the balance' do
       expect(subject.balance).to eq 0
     end
   end
 
-
-  context 'Adding money and setting the balance limit'
-  describe '#top_up' do
-    it 'adds money' do
-      expect(subject).to respond_to(:top_up).with(1).argument
+  describe '#top_up - adds money, sets balance limit' do
+    context 'when total amount less than balance limit' do
+      it 'adds money' do
+        expect(subject).to respond_to(:top_up).with(1).argument
+      end
     end
 
-    it 'sets the limit at £90' do
-      maximum_balance = Oystercard::LIMIT
-      subject.top_up(maximum_balance)
-      expect { subject.top_up(1) }.to raise_error "You have exceeded your £#{maximum_balance} limit!"
+    context 'when total amount over balance limit' do
+      it 'raises balance limit error' do
+        maximum_balance = Oystercard::LIMIT
+        subject.top_up(maximum_balance)
+        expect { subject.top_up(1) }.to raise_error "You have exceeded your £#{maximum_balance} limit!"
+      end
     end
   end
 
-  context 'Paying the fare and deducting from the balance'
-
-  describe '#pay_fare' do
-    it { should respond_to(:pay_fare).with(1).argument }
+  describe '#pay_fare - deducts money from balance' do
+    it 'deducts money from the balance' do
+      expect(subject.pay_fare(5)).to eq -5
+    end
   end
 
-  it 'deducts money from the balance' do
-    expect(subject.pay_fare(5)).to eq -5
-  end
-
-
-  context 'journey sequence'
-
-  describe '#in_journey?' do
-    it { should respond_to(:in_journey?) }
-  end
-
+  # context 'journey sequence'
   describe '#touch_in' do
-    let(:station) { double :station }
-
     it 'is initially not in journey' do
       expect(subject).not_to be_in_journey
     end
@@ -55,43 +48,41 @@ describe Oystercard do
     it 'prevents journey if insufficient balance' do
       expect { subject.touch_in(station) }.to raise_error("Insufficient balance!")
     end
+    context 'touches in succesfully' do
+      before do
+        subject.top_up(5)
+        subject.touch_in(station)
+      end
+      it "stores the entry station" do
+        expect(subject.journey_hash).to eq({entry_station: station})
+      end
 
-    it "stores the entry station" do
-      subject.top_up(5)
-      subject.touch_in(station)
-      expect(subject.journey_hash).to eq({entry_station: station})
-    end
-
-    it 'is in journey' do
-      subject.top_up(5)
-      subject.touch_in(station)
-      expect(subject).to be_in_journey
+      it 'is in journey' do
+        expect(subject).to be_in_journey
+      end
     end
   end
 
-  describe '#touch_out' do
-    let(:station) { double :station }
-    let(:exit_station) { double :station }
-    let(:entry_station) { double :station }
-    it 'ends the journey' do
-      subject.top_up(5)
-      subject.touch_in(entry_station)
-      expect { subject.touch_out(exit_station) }.to change { subject.balance }.by(-Oystercard::MINIMUM)
-    end
-
-    it 'ends the journey' do
+  describe '#touch_out - ends journey, stores station and clears journey hash' do
+    before do
       subject.top_up(5)
       subject.touch_in(entry_station)
       subject.touch_out(exit_station)
+    end
+
+    it 'deducts the fare from the balance' do
+      expect(subject.balance).to eq(3)
+    end
+
+    it 'sets in_journey to false' do
       expect(subject).not_to be_in_journey
+    end
+
+    it 'adds station to :exit_station' do
       expect(subject.journey_history).to eq [{entry_station: entry_station, exit_station: exit_station}]
     end
 
     it 'clears journey hash' do
-      subject.top_up(5)
-      subject.touch_in(entry_station)
-      subject.touch_out(exit_station)
-      expect(subject).not_to be_in_journey
       expect(subject.journey_hash).to eq nil
     end
   end
